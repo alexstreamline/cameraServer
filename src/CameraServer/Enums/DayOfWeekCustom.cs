74243b90b17daab0d9e079252d161644da4b59c8
Alex_Koshkin <alekseykoshkin@rambler.ru>
2016-12-28 00:25:03 +0300

    енумы для дней недели и датчиков

---
+++ src/CameraServer/Enums/DayOfWeekCustom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CameraServer.Enums
+{
+    public enum DayOfWeekCustom
+    {
+        [Display(Name = "Понедельник")]
+        Monday = 1,
+        [Display(Name = "Вторник")]
+        Tuesday,
+        [Display(Name = "Среда")]
+        Wednesday,
+        [Display(Name = "Четверг")]
+        Thursday,
+        [Display(Name = "Пятница")]
+        Friday,
+        [Display(Name = "Суббота")]
+        Saturday,
+        [Display(Name = "Воскресенье")]
+        Sunday 
+    }
+}
---
+++ src/CameraServer/Enums/DeviceSensorName.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CameraServer.Enums
+{
+    public enum DeviceSensorName
+    {
+        // [Display(Name = "Motion1")]
+        [Display(Name = "Датчик движения 1")]
+        Motion1 = 1,
+        [Display(Name = "Датчик движения 2")]
+        Motion2,
+        [Display(Name = "Датчик движения 3")]
+        Motion3,
+        [Display(Name = "Датчик движения 4")]
+        Motion4,
+        [Display(Name = "Компас")]
+        Compass,
+        [Display(Name = "Гироскоп")]
+        Gyroscope,
+        [Display(Name = "Акселерометр")]
+        Accelerometer,
+        [Display(Name = "Барометр")]
+        Barometer,
+        [Display(Name = "GPS/Глонасс")]
+        Gps,
+        [Display(Name = "Термометр")]
+        Temperature,
+        [Display(Name = "Датчик влажности")]
+        Humidity,
+        [Display(Name = "Датчик вибрации")]
+        Vibration
+            
+        
+    }
+}
